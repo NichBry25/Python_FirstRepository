7d716fb37f9cd2b3a4661d9dc3b0f9c267d915c0
NichBry25 <nicholas.bryan001@binus.ac.id>
2024-09-18 16:44:37 +0700

    Add files via upload

---
+++ pizza.py
@@ -0,0 +1,6 @@
+pizza = ["Margherita", "Pepperoni", "American Fav", "Hawaiian Pizza", "Sausage", "Affordable Pizza", "Papa's Za"]
+
+for slices in pizza:
+    print(f"I very much like {slices}")
+
+print("I really love pizza! \nIt is one of my comfort foods! \nYou should eat more pizza. :)")
---
+++ stagesoflife.py
@@ -0,0 +1,20 @@
+# If Statements
+# Stages of Life
+
+def StagesOfLife(age):
+    if age < 2:
+        print("Baby")
+    elif 2 <= age < 4:
+        print("Toddler")
+    elif 4 <= age < 13:
+        print("Kid")
+    elif 13 <= age < 20:
+        print("Teenager")
+    elif 20 <= age < 65:
+        print("Adult")
+    elif age >= 65 :
+        print("Elder")
+    else:
+        print("Woop")
+    
+StagesOfLife(100)
